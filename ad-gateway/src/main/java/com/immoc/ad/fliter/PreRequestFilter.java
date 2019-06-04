c4de10d3990d0bc2b7b9fed6ceb808b08ff97126
yangbao <yangbaobgkp@sina.com>
2019-06-04 12:47:03 +0800

    完成API-gateway的开发

---
+++ ad-gateway/src/main/java/com/immoc/ad/fliter/PreRequestFilter.java
@@ -0,0 +1,33 @@
+package com.immoc.ad.fliter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PreRequestFilter extends ZuulFilter{
+    @Override
+    public String filterType() {
+        return FilterConstants.PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return FilterConstants.SERVLET_DETECTION_FILTER_ORDER-1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        //记录请求开始的时间戳
+        RequestContext context = RequestContext.getCurrentContext();
+        context.set("request_start_time",System.currentTimeMillis());
+        return null;
+    }
+}
